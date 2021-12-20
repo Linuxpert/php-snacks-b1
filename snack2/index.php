e2c539cb97c4182693aadb1b4a8af8d61b961f28
Linuxpert <g.manfre10@gmail.com>
2021-12-20 16:40:05 +0100

    snack 2 completato

--- snack2/index.php
+++ snack2/index.php
@@ -8,5 +8,30 @@
 </head>
 <body>
     
+    <?php
+
+        $nameUser = $_GET["nome"];
+        $mailUser = $_GET["mail"];
+        $ageUser = $_GET["age"];
+
+        $at = strpos($mailUser , "@");
+        $dot = strpos($mailUser , ".");
+        $isMailOk = $at !== false && $dot !== false;
+
+        $ageNumber = is_numeric($ageUser);
+    ?>
+
+    <h2>
+        <?php
+
+            echo strlen($nameUser) . "<br>";
+            if(strlen($nameUser) > 3 && $isMailOk && $ageNumber){
+                echo "Accesso riuscito";
+            }else {
+                echo "Accesso negato";
+            };
+        ?>
+    </h2>
+
 </body>
 </html>
