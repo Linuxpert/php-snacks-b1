487b68a5f3b5338ff246e2626744a702a0ca9d23
Linuxpert <g.manfre10@gmail.com>
2021-12-20 16:16:25 +0100

    snack 1 completato

--- snack1/index.php
+++ snack1/index.php
@@ -8,5 +8,50 @@
 </head>
 <body>
     
+    <?php
+        $arreyMatch = [
+            [
+                "homeTeam" => "Olimpia",
+                "awayTeam" => "Cantù",
+                "homePoints" => "55",
+                "awayPoint" => "60",
+            ],
+            [
+                "homeTeam" => "Lakers",
+                "awayTeam" => "Chicago Bulls",
+                "homePoints" => "102",
+                "awayPoint" => "98",
+            ],
+            [
+                "homeTeam" => "Boston Celtics",
+                "awayTeam" => "New York Knicks",
+                "homePoints" => "104",
+                "awayPoint" => "56",
+            ],
+            [
+                "homeTeam" => "Cleveland",
+                "awayTeam" => "Houston Rockets",
+                "homePoints" => "110",
+                "awayPoint" => "109",
+            ],
+            [
+                "homeTeam" => "Miami Heat",
+                "awayTeam" => "Orlando Magic",
+                "homePoints" => "45",
+                "awayPoint" => "69",
+            ],
+        ];
+
+        // var_dump($arreyMatch);
+        echo "<br>";
+        
+        for($i=0; $i<count($arreyMatch); $i++){
+
+            echo $arreyMatch[$i]["homeTeam"] . " -" . $arreyMatch[$i]["awayTeam"] . " | " . $arreyMatch[$i]["homePoints"] . "-" . $arreyMatch[$i]["awayPoint"] . "<br>";
+            // echo $arreyMatch[$i]["homeTeam"] . " - " . $arreyMatch[$i]["awayTeam"];
+        };
+    ?>
+
+
 </body>
 </html>
